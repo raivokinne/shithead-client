803387927185a1d219a54c15017a407b74057793
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2024-12-18 20:05:14 +0200

    update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'production'
+    ? '.env.production'
+    : '.env.development'
+});
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTQx5rFzpxG-E242aX4mrZPMjvlWMoYbM",
-  authDomain: "shithead-fca74.firebaseapp.com",
-  projectId: "shithead-fca74",
-  storageBucket: "shithead-fca74.firebasestorage.app",
-  messagingSenderId: "1055638074991",
-  appId: "1:1055638074991:web:085358fe9c69b42f36ff9a",
-  measurementId: "G-P68GK1V75B"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
 export const auth = getAuth(app);
