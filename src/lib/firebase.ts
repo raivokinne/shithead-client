75c555d53b863b2d08a7140e17d20471f610beb8
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2024-12-18 19:49:30 +0200

    update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDTQx5rFzpxG-E242aX4mrZPMjvlWMoYbM",
+  authDomain: "shithead-fca74.firebaseapp.com",
+  projectId: "shithead-fca74",
+  storageBucket: "shithead-fca74.firebasestorage.app",
+  messagingSenderId: "1055638074991",
+  appId: "1:1055638074991:web:085358fe9c69b42f36ff9a",
+  measurementId: "G-P68GK1V75B"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
