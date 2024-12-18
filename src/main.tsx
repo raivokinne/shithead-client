a28be401e38488df4a5be72102b227dfcd8326ff
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2024-12-18 14:40:43 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { Toaster } from './components/ui/toaster';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster />
     <RouterProvider router={router} />
+    <Analytics />
   </StrictMode>,
 )
