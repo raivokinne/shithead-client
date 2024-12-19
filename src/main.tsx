bb712e19581e7415673bc9612475007541a6f35b
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2024-12-19 22:05:53 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { Toaster } from "./components/ui/toaster";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
