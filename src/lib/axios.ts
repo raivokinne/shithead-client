431e3ba14531174a8b3c584b4a8e0af9ca06aa5e
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2025-01-12 19:15:15 +0200

    update

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const instance = axios.create({
-	// baseURL: "https://api.troika.id.lv",
+	baseURL: "https://api.troika.id.lv",
 	// baseURL: "http://localhost:8000",
-	baseURL: "http://192.168.8.108:8000",
+	// baseURL: "http://192.168.8.108:8000",
 	headers: {
 		"Content-Type": "application/json",
 		Accept: "application/json",
